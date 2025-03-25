248208d653e1774699464fa229a647131b1c7571
BP1009 <bp1009@erlanakbaraliev.local>
2025-03-25 23:07:41 +0100

    #1: Added Header, Main, Footer components, index.css style

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,8 +1,19 @@
-
-
 function App() {
   return (
-      <h1>ss</h1>
+    <>
+      <main>
+        <h1>Fun facts about React</h1>
+        <ol>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 200K stars on GitHub</li>
+        </ol>
+      </main>
+
+      <footer>
+        <small>@ 20xx last_name development. All rights reserved.</small>
+      </footer>
+    </>
   );
 }
 
